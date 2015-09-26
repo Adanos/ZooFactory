365aa5a1874793ed53133ebba4357a6aef352dd4
Adanos <polon1@op.pl>
2015-09-26 23:33:42 +0200

    Task #2

--- ZooFactory/factories/LionFactory.cs
+++ ZooFactory/factories/LionFactory.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZooFactory.interfaces;
+using ZooFactory.products;
 
 namespace ZooFactory.factories
 {
 	class LionFactory : ZooFactory
 	{
+		public override IAnimalProduct createProduct()
+		{
+			return new Lion();
+		}
 	}
 }
